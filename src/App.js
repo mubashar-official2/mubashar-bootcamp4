46f75d95dc90a5c3b404f74897de6fdd91089b97
mubashar <mubashar.hussain22@gmail.com>
2020-11-07 08:22:19 +0500

    finlazed bootcamp4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React , {useState} from 'react';
+import {Message} from './Message.js';
 import './App.css';
 
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+  let [count,setCount] = useState(0);
+  let [count2,setCount2] = useState(0);
+  let [ismorning, setmorning] =useState(true);
+      // count if varialbe and setCount is its setter
+      // in class we could not used this sentyx and used old method used in class form
+      // this variable not save when brower refresh it will distroy 
+  return (            
+            <div className = {`box ${ismorning ? 'dayLight' : ''}`}>
+              <h3> Day time is = {ismorning ? 'Morning' : 'Night'} </h3>
+                  <h3>Parent Component counter {count2} </h3>
+                  <Message  counter={count}/>
+                  
+                    <button onClick={
+                                        ()=>setCount(count = count + 1)
+                                    }
+                    > Click Set to child component value 
+                    </button>
+                    <br/>
+
+                    <button onClick={
+                                    // ()=> alert("Button have clicked") 
+                                    //()=> setCount(count + 1)
+                                    //()=> setCount(count++) // postfix will not work
+                                    ()=> setCount2(++count2)
+                                    }
+                      > Click button Parent component </button> 
+                      <br/>
+                      <button onClick={
+                                        ()=>setmorning(true)
+                                      }> 
+                                    Click to Morning 
+                                      </button>
+                                      <br/>
+                      <button onClick={
+                                        ()=>setmorning(false)
+                                      }> 
+                                    Click to Night Mood 
+                                      </button>
+                                  
+                                  
+            </div>
   );
 }
+
+// old method or render spcific part of app
+// class Appclass extends React.Component
+// {
+//   this.status = {}
+//   componentDidMount
+/* <h3> this counter {count} </h3> */
+
+
+//   render
+// } 
 
 export default App;
---
+++ src/Message.js
@@ -0,0 +1,5 @@
+import React from 'react';
+export function Message(props)
+{
+    return( <h3> Child Component counter {props.counter} </h3>)
+}
